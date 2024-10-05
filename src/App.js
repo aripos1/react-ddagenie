c745de463f40f5603be7df70e046d72c501ba852
함민규 <antrophus@yahoo.com>
2024-10-05 11:17:31 +0900

    loginforn 리엑트 전환중

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Test from './pages/Test';
 import MusicPlayer from './pages/music/MusicPlayer';
 import MyMusic from './pages/music/MyMusic';
+import LoginForm from './pages/user/LoginForm';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path='/test' element={<Test />} />
           <Route path='/music/musicplayer' element={<MusicPlayer />} />
           <Route path='/user/mymusic' element={<MyMusic />} />
+          <Route path='/login' element={<LoginForm />} />
         </Routes>
       </BrowserRouter>
     </div>
