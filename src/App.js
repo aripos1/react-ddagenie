c73f8193cc13eb3e9a783510b9ff5809484883f6
jiny0852 <jiny0852@gmail.com>
2024-10-05 11:23:29 +0900

    얍

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Test from './pages/Test';
 import MusicPlayer from './pages/music/MusicPlayer';
 import MyMusic from './pages/music/MyMusic';
 
+
+import MusicAdmin from './pages/admin/MusicAdmin';
+
 function App() {
   return (
     <div>
@@ -11,6 +14,10 @@ function App() {
           <Route path='/test' element={<Test />} />
           <Route path='/music/musicplayer' element={<MusicPlayer />} />
           <Route path='/user/mymusic' element={<MyMusic />} />
+
+
+          <Route path='/admin/musicadmin' element={<MusicAdmin />} />
+
         </Routes>
       </BrowserRouter>
     </div>
