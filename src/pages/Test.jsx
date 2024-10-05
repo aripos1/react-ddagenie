861cc650b6ec97a74585b9682184736f5a96ba02
aripos1 <ihsjjang@gmail.com>
2024-10-05 10:55:52 +0900

    test 연동

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,20 +1,33 @@
-//import 라이브러리
-import React from 'react';
+// import 라이브러리
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 //import 컴포넌트
 
 //import css
 const Test = () => {
     /*---상태관리 변수들(값이 변화면 화면 랜더링) ----------*/
+    const [message, setMessage] = useState('');
 
     /*---일반 메소드 --------------------------------------------*/
+
     /*---생명주기 + 이벤트 관련 메소드 ----------------------*/
+    useEffect(() => {
+        // Spring Boot API 호출
+        axios.get('http://localhost:8888/api/test')
+            .then(response => {
+                setMessage(response.data); // API로부터 받은 데이터를 상태로 설정
+            })
+            .catch(error => {
+                console.error("There was an error!", error);
+            });
+    }, []);
+
     return (
         <>
-         
-                이름출력
-       
+            <h1>{message}</h1>
         </>
     );
 }
+
 export default Test;
