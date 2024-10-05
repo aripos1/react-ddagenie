b3478986f3411fd48d559dcecf99a72012b1cd25
aripos1 <ihsjjang@gmail.com>
2024-10-05 17:52:14 +0900

    123

--- src/App.js
+++ src/App.js
@@ -17,14 +17,10 @@ function App() {
           <Route path='/testlist' element={<TestList />} />
           <Route path='/music/musicplayer' element={<MusicPlayer />} />
           <Route path='/user/mymusic' element={<MyMusic />} />
-<<<<<<< HEAD
 
 
           <Route path='/admin/musicadmin' element={<MusicAdmin />} />
 
-=======
-          <Route path='/login' element={<LoginForm />} />
->>>>>>> c745de463f40f5603be7df70e046d72c501ba852
         </Routes>
       </BrowserRouter>
     </div>
