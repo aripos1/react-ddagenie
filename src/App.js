938afff34195388fbe188a7cf0a6579fcec88039
aripos1 <ihsjjang@gmail.com>
2024-10-05 11:51:41 +0900

    testlist

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Test from './pages/Test';
 import MusicPlayer from './pages/music/MusicPlayer';
 import MyMusic from './pages/music/MyMusic';
 import LoginForm from './pages/user/LoginForm';
+import TestList from './pages/TestList';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/test' element={<Test />} />
+          <Route path='/testlist' element={<TestList />} />
           <Route path='/music/musicplayer' element={<MusicPlayer />} />
           <Route path='/user/mymusic' element={<MyMusic />} />
           <Route path='/login' element={<LoginForm />} />
